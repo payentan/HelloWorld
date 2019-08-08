dc1643c07c67c63a1339923a26fe86d61720bfc9
root <payentan@hotmail.com>
2019-08-09 00:34:23 +0800

    build static and shared library

---
+++ cmake-general/src/staticlib/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello_static.h"
+
+void HelloStatic()
+{
+    cout<<"Hello World from static library"<<endl;
+}
---
+++ cmake-general/src/staticlib/hello_static.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_STATIC_H_
+#define HELLO_STATIC_H_
+
+#include <iostream>
+
+using namespace std;
+
+void HelloStatic();
+
+#endif
